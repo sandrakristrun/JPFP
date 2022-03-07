25e1d5f7f4a6d07d2f2634ca4fe7db3cb4ba451c
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-03-07 15:59:50 -0500

    server fixes

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -1,53 +1,53 @@
 const router = require("express").Router();
-const Campus = require('../db/models/campus')
-const Student = require('../db/models/student')
+const Campus = require("../db/models/campus");
+const Student = require("../db/models/student");
 
 //GET /api/campuses
-router.get('/', async(req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const allCampuses = await Campus.findAll({
       include: {
-        model: Student
-      }
+        model: Student,
+      },
     });
-    res.send(allCampuses)
-  } catch (error){
-    next(error)
+    res.send(allCampuses);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 //POST /api/campuses
-router.post('/addCampus', async(req, res, next) => {
+router.post("/addCampus", async (req, res, next) => {
   try {
     const newCampus = await Campus.create(req.body);
-    res.send(newCampus)
-  } catch (error){
-    next(error)
+    res.send(newCampus);
+  } catch (error) {
+    next(error);
   }
-})
+});
 //GET /api/campuses/:campusId
-router.get('/:campusId', async(req, res, next) => {
+router.get("/:campusId", async (req, res, next) => {
   try {
     const id = req.params.campusId;
     const campus = await Campus.findOne({
       where: {
-        id: id
+        id: id,
       },
       include: {
-        model: Student
-      }
+        model: Student,
+      },
     });
-    res.send(campus)
-  } catch (error){
-    next(error)
+    res.send(campus);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 // PUT /api/campuses/editCampus/:campusId
-router.put('/editCampus/:campusId', async (req, res, next) => {
+router.put("/editCampus/:campusId", async (req, res, next) => {
   try {
     const campus = await Campus.findByPk(req.params.campusId);
-    const updated = await campus.update(req.body)
+    const updated = await campus.update(req.body);
     res.send(updated);
   } catch (error) {
     next(error);
@@ -55,14 +55,14 @@ router.put('/editCampus/:campusId', async (req, res, next) => {
 });
 
 //DELETE /api/campuses/:campusId
-router.delete('/:campusId', async(req, res, next) => {
+router.delete("/:campusId", async (req, res, next) => {
   try {
     const campus = await Campus.findByPk(req.params.campusId);
     await campus.destroy();
     res.send(campus);
-  } catch (error){
-    next(error)
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- server/api/students.js
+++ server/api/students.js
@@ -1,51 +1,51 @@
 const router = require("express").Router();
 
-const Student = require('../db/models/student')
-const Campus = require('../db/models/campus')
+const Student = require("../db/models/student");
+const Campus = require("../db/models/campus");
 
 //GET /api/students
-router.get('/', async(req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const allStudents = await Student.findAll();
-    res.send(allStudents)
-  } catch (error){
-    next(error)
+    res.send(allStudents);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 //POST /api/students
-router.post('/addStudent', async(req, res, next) => {
+router.post("/addStudent", async (req, res, next) => {
   try {
     const newStudent = await Student.create(req.body);
-    res.send(newStudent)
-  } catch (error){
-    next(error)
+    res.send(newStudent);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 //GET /api/students/:studentId
-router.get('/:studentId', async(req, res, next)=> {
+router.get("/:studentId", async (req, res, next) => {
   try {
     const id = req.params.studentId;
     const student = await Student.findOne({
       where: {
-        id: id
+        id: id,
       },
       include: {
-        model: Campus
-      }
+        model: Campus,
+      },
     });
-    res.send(student)
-  } catch (error){
-    next(error)
+    res.send(student);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 // PUT /api/students/editStudent/:studentId
-router.put('/editStudent/:studentId', async (req, res, next) => {
+router.put("/editStudent/:studentId", async (req, res, next) => {
   try {
     const student = await Student.findByPk(req.params.studentId);
-    const updated = await student.update(req.body)
+    const updated = await student.update(req.body);
     res.send(updated);
   } catch (error) {
     next(error);
@@ -53,14 +53,14 @@ router.put('/editStudent/:studentId', async (req, res, next) => {
 });
 
 //DELETE /api/students/:studentId
-router.delete('/:studentId', async(req, res, next) => {
+router.delete("/:studentId", async (req, res, next) => {
   try {
     const student = await Student.findByPk(req.params.studentId);
     await student.destroy();
     res.send(student);
-  } catch (error){
-    next(error)
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
