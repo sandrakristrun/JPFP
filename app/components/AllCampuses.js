dade44dd5e3469691b0aa471748b86c4107ecabc
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-03-07 15:54:55 -0500

    small fixes

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom'
 import { fetchCampuses, deleteCampus } from "../redux/campuses";
 import AddCampus from "./AddCampus";
 
-// Notice that we're exporting the AllCampuses component twice. The named export
-// (below) is not connected to Redux, while the default export (at the very
-// bottom) is connected to Redux. Our tests should cover _both_ cases.
+
 export class AllCampuses extends React.Component {
   constructor(){
     super()
